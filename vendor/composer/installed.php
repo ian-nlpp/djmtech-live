cdf456b95a55234cf211940149e61e93c58809e6
Ian Neil Pepe <ianpepe99@gmail.com>
2025-03-08 03:33:56 -0700

    Installed Composer - PHPMailer
    composer require phpmailer/phpmailer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dev/djmtech',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2e32606b1045d5846a82fc8fb2952594eb497499',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dev/djmtech' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2e32606b1045d5846a82fc8fb2952594eb497499',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.9.3',
+            'version' => '6.9.3.0',
+            'reference' => '2f5c94fe7493efc213f643c23b1b1c249d40f47e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
